0bb8a574af9ae623db6e9f786da8792471733173
Gustavo Teles <gustavo.teles711@gmail.com>
2023-11-07 21:47:24 -0300

    commit inicial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,11 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Dicionario',
   description: 'Dicionario online, busque o significado das palavras',
+  icons: {
+    icon: "/favicon.ico?v=4",
+    apple: "/apple-touch-icon.png?v=4",
+    shortcut: "/apple-touch-icon.png"
+  },
 }
 
 export default function RootLayout({
