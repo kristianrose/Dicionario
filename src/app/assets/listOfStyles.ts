4030825ac08460ef025372dae3054d0523f4d761
Gustavo Teles <gustavo.teles711@gmail.com>
2023-11-07 21:19:09 -0300

    commit inicial

---
+++ src/app/assets/listOfStyles.ts
@@ -0,0 +1,14 @@
+export default {
+  veryBlack: "#050505",
+  black: "#1f1f1f",
+  lightBlack: "#2d2d2d",
+  veryLightBlack: "#3a3a3a",
+
+  gray: "#757575",
+  lightGray: "#e9e9e9",
+  white: "#f4f4f4",
+  veryLight: "#ffffff",
+
+  purple: "#a445ed",
+  red: "#ff5252",
+};
